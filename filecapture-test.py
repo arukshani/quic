4b56b2fd3191d72d28e3f494be62b0514a3d0d35
Rukshani <rukz.atha@gmail.com>
2022-05-29 10:51:36 -0700

    first commit

---
+++ filecapture-test.py
@@ -0,0 +1,64 @@
+# importing the pyshark module
+import socket
+from struct import pack
+import pyshark
+import json
+
+# capture on the first wifi interface
+iface_name = 'en0'
+# targeting all HTTPS traffic on port 443
+filter_string = 'port 443'
+
+# building our live capture instance
+capture = pyshark.FileCapture(input_file="/Users/rukshani/Documents/code-227/fingerprint/chrome_cloudflare.pcapng")
+
+# for capturing the traffic
+# timeout of 5 seconds and a limit of 10 packets total
+# capture.sniff(timeout=5, packet_count=10)
+def proto_name_by_num(proto_num):
+    for name,num in vars(socket).items():
+        if name.startswith("IPPROTO") and proto_num == num:
+            return name[8:]
+    return "Protocol not found"
+
+def writeByteStringToFile(out, data):
+    out.write(json.dumps(data))
+    out.write("\n")
+
+filename = "chrome_cloudflare.json"
+out = open(filename, 'w')
+i = 1
+j = 1
+l = 0
+max_l = 0
+max_l_fields = ""
+min_l_fields = ""
+min_l = 10000
+max_pos = 0
+min_pos = 0
+for packet in capture:
+    if(packet.__contains__("quic")):
+        
+        if i == 0 or i == 1:
+            writeByteStringToFile(out, packet.quic.__dict__["_all_fields"])
+            # print(i, "###############", packet.quic.__dict__["_all_fields"])
+        l = len(packet.quic.__dict__["_all_fields"])
+        if max_l < l:
+            max_l = l
+            max_l_fields = packet.quic.__dict__["_all_fields"]
+            max_pos = i
+        if min_l > l:
+            min_l = l
+            min_l_fields = packet.quic.__dict__["_all_fields"]
+            min_pos = i
+        i+=1
+    j+=1
+
+# print("i = ", i, " j = ", j, " l = ", l)
+# print("max_l = ", max_l, " min_l = ", min_l)
+# print("max_pos = ", max_pos, " min_pos = ", min_pos)
+# print("max_l_fields = ", max_l_fields.keys(), " min_l_fields = ", min_l_fields.keys())
+
+
+
+
---
+++ readparas.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import json
+
+def read_paras():
+    # df = pd.read_json('./example-del.json')
+    # print(df[0])
+    with open('chrome_cloudflare.json') as f:
+        data = json.load(f)
+    print("---Chrome cloudflare---")
+    print("initial_max_stream_data_uni: ", data['tls.quic.parameter.initial_max_stream_data_uni'])
+    print("max_datagram_frame_size: ",data['tls.quic.parameter.max_datagram_frame_size'])
+    print("initial_source_connection_id: ", data['tls.quic.parameter.initial_source_connection_id'])
+    print("initial_max_streams_uni: ",data['tls.quic.parameter.initial_max_streams_uni'])
+    # print(data['tls.quic.parameter.grease_quic_bit'])
+    print("initial_max_stream_data_bidi_local: ", data['tls.quic.parameter.initial_max_stream_data_bidi_local'])
+    # print(data['tls.quic.parameter.GREASE'])
+    print("initial_max_data: ",data['tls.quic.parameter.initial_max_data'])
+    # print("active_connection_id_limit: ", data['tls.quic.parameter.active_connection_id_limit'])
+    print("initial_max_stream_data_bidi_remote: ",data['tls.quic.parameter.initial_max_stream_data_bidi_remote'])
+    # print(data['tls.quic.parameter.disable_active_migration'])
+    print("max_idle_timeout: ", data['tls.quic.parameter.max_idle_timeout'])
+    print("initial_max_streams_bidi: ", data['tls.quic.parameter.initial_max_streams_bidi'])
+
+read_paras()
